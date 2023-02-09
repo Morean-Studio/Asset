bafba481a5293b9c1fdee216499bb0d714f6707a
Besjan <xhikab@gmail.com>
2023-02-09 23:57:15 +0100

    Refactor

--- Extensions.cs
+++ Extensions.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
-using UnityEngine.ResourceManagement.ResourceProviders;
 using UnityEngine.SceneManagement;
 
 namespace Morean.Assets
